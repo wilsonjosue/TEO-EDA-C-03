757e2ac52856caef8b9d955842c098b2aa8f772a
Wilson Turpo Huanca <wturpoh@unsa.edu.pe>
2023-06-17 06:54:28 -0500

    Instaciamos las clases

---
+++ TestHeaps.java
@@ -0,0 +1,11 @@
+ 
+ public class TestHeaps {
+
+    public static void main(String[] args) {
+        Heap<Integer> monticulo = new Heap<>();
+        
+        PriorityQueueHeap<Integer> heapPrioritario = new PriorityQueueHeap<>();
+        // Agregar elementos a la cola de prioridad
+
+   }
+}
